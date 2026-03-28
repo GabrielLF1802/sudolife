ac62bdf9e506a26bb5d57ae2350a7ff188bed24c
gabriel <gabriellourencofaria@gmail.com>
2026-03-28 14:42:02 -0300

    initializing the project

---
+++ src/main/java/com/sudolife/SudolifeApplication.java
@@ -0,0 +1,13 @@
+package com.sudolife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudolifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudolifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudolife/SudolifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudolife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudolifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
